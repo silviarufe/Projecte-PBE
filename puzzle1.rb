a96ca21ca5bdb6eca1a7614f21957efe0e828fb7
silviarufe <silviarufe@gmail.com>
2024-10-05 12:31:38 +0200

    Add files via upload

---
+++ puzzle1.rb
@@ -0,0 +1,24 @@
+require 'ruby-nfc'
+
+class Rfid
+
+  def read_uid
+
+	nfc = NFC::Reader.all
+
+  	nfc[0].poll(Mifare::Classic::Tag) do |tag|
+		return uid_hex.upcase
+  	
+	end
+  end
+end
+
+if __FILE__ == $0
+  rf = Rfid.new
+  uid = rf.read_uid
+  puts uid
+end
+
+
+
+
